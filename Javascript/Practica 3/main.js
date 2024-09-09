3560bdcabadb15f6d1d4890e68a50ed4d01031c8
Fedemello1224 <162213626+Fedemello1224@users.noreply.github.com>
2024-09-09 20:24:02 -0300

    Prog4

---
+++ Javascript/Practica 3/main.js
@@ -0,0 +1,61 @@
+//Ejercicio 7
+
+/*let num1 = parseInt(prompt("Ingrese el valor 1", 0));
+let num2 = parseInt(prompt("Ingrese el valor 2", 0));
+let num3 = parseInt(prompt("Ingrese el valor 3", 0));
+if(num1 > num2 && num1 > num3)
+{
+    Mayor=num1;
+}
+else if(num2 > num3)
+{
+    Mayor = num2;
+}
+else
+{
+    Mayor = num3;
+}
+
+
+console.log("El numero mayor es:", Mayor);
+document.write("El numero mayor es:", Mayor);*/
+
+//Ejercicio 8
+
+/*let num1 = parseInt(prompt("Ingrese un valor", 0));
+
+if(num1 % 2 === 0)
+{
+    console.log("El numero",num1,"es divisible por 2");
+    document.write("El numero",num1,"es divisible por 2");
+}
+else
+{
+    console.log("El numero",num1,"no es divisible por 2");
+    document.write("El numero",num1," no es divisible por 2");
+}*/
+
+//Ejercicio 13 y 14
+
+let num1 = parseInt(prompt("Ingrese un valor", 0));
+
+if(num1 % 2 === 0)
+    {
+        console.log("El numero",num1,"es divisible por 2");
+        document.write("El numero",num1,"es divisible por 2");
+    }
+if(num1 % 3 === 0)   
+    {
+        console.log("El numero", num1,"es divisible por 3");
+        document.write("El numero",num1,"es divisible por 3");
+    }
+if(num1 % 5 === 0)   
+    {
+        console.log("El numero", num1,"es divisible por 5");
+        document.write("El numero",num1,"es divisible por 5");
+    }          
+if(num1 % 7 === 0)   
+    {
+        console.log("El numero", num1,"es divisible por 7");
+        document.write("El numero",num1,"es divisible por 7");
+    }       
--- Javascript/Practica2/main.js
+++ Javascript/Practica2/main.js
@@ -6,14 +6,32 @@ let pass = prompt("Ingrese la Contraseña","");
 let num1 = parseInt(prompt("Ingrese el valor 1", 0));
 let num2 = parseInt(prompt("Ingrese el valor 2", 0));
 
+if(num1 > num2)
+{
+    Mayor=num1;
+}
+else
+{
+    Mayor = num2;
+}
+
+console.log("El numero mayor es:",Mayor);
 
 const sum = num1 + num2;
 const rest = num1 - num2;
 const multi = num1 * num2;
 const div = num1 / num2;
 
-document.write("Bienvenido" +" ",usser,"<br/>")
+console.log("Bienvenido" +" ",usser);
+console.log(num1, "+", num2,"=", sum );
+console.log(num1,"-",num2,"=", rest);
+console.log(num1,"*",num2,"=", multi);
+console.log(num1,"/",num2,"=", div);
+
+document.write("Bienvenido" +" ",usser,"<br/>");
 document.write(num1, "+", num2,"=", sum,"<br/>" );
 document.write(num1,"-",num2,"=", rest,"<br/>");
 document.write(num1,"*",num2,"=", multi,"<br/>");
 document.write(num1,"/",num2,"=", div,"<br/>");
+
+document.write("El numero mayor es:",Mayor);
