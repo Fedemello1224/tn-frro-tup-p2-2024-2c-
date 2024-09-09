6bf30c9ba81f75dd3af15cb62f1bff591be7855a
Fedemello1224 <162213626+Fedemello1224@users.noreply.github.com>
2024-09-09 20:57:01 -0300

    Update main.js

--- Javascript/Practica 3/main.js
+++ Javascript/Practica 3/main.js
@@ -16,7 +16,6 @@ else
     Mayor = num3;
 }
 
-
 console.log("El numero mayor es:", Mayor);
 document.write("El numero mayor es:", Mayor);*/
 
@@ -37,7 +36,7 @@ else
 
 //Ejercicio 13 y 14
 
-let num1 = parseInt(prompt("Ingrese un valor", 0));
+/*let num1 = parseInt(prompt("Ingrese un valor", 0));
 
 if(num1 % 2 === 0)
     {
@@ -58,4 +57,50 @@ if(num1 % 7 === 0)
     {
         console.log("El numero", num1,"es divisible por 7");
         document.write("El numero",num1,"es divisible por 7");
-    }       
+    } 
+*/
+//Ejercicio 15
+
+/*let num1 = parseInt(prompt("Ingrese un valor", 0));
+document.write("Los numeros por los que ", num1, " es divisible son:");
+for ( i= 0; i <= num1; i++)
+{
+    if(num1 % i === 0)
+    {
+        document.write(",",i);
+    }
+}
+*/
+
+//Ejercicio 16
+
+/*var num1 = parseInt(prompt("Ingrese un valor", 0));
+var num2 = parseInt(prompt("Ingrese un valor", 0));
+if (num1 < num2)
+{
+    num3=num1;
+}
+else
+{
+    num3=num2;
+}
+document.write("Los numeros por los que "+ num1 +" y "+ num2 +" son divisibles son:");
+for ( i= 0; i <= num3; i++)
+{
+    if(num1 % i === 0 && num2 % i === 0)
+    {
+        document.write(",",i);
+    }
+}
+*/
+
+//Ejercicio 17
+
+
+
+
+
+
+
+
+
