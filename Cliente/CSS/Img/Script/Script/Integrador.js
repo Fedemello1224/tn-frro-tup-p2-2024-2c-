39c6721e78f50ff26161122045e5c9d4a2957b2b
Fedemello1224 <162213626+Fedemello1224@users.noreply.github.com>
2024-11-11 16:49:17 -0300

    Arreglos e historico tira error el get de la api historico con la fecha hay que arreglarlo

---
+++ Cliente/CSS/Img/Script/Script/Integrador.js
@@ -0,0 +1,70 @@
+const crearCartaCotizacion = (cotizacion) => {
+  
+    const carta = document.createElement('div');
+    carta.classList.add('dolar-oficial');
+  
+    carta.innerHTML = `  
+      <div class="dolar">${cotizacion.nombre} ${cotizacion.tipoDeCambio}</div>
+      <div class="compra-venta">
+          <div class="compra">
+              <span>Compra</span>
+              <span class="precio-compra">$${parseFloat(cotizacion.compra).toFixed(2)}</span>
+          </div>
+          <div class="venta">
+              <span>Venta</span>
+              <span class="precio-venta">$${parseFloat(cotizacion.venta).toFixed(2)}</span>
+          </div>
+      </div>
+    `;
+    // que complicado es hacer un backstick 
+  
+    return carta; 
+  };
+  
+  const actualizarCotizaciones = (cotizaciones) => {
+    const container = document.querySelector('.carta-container');
+    container.innerHTML = '';
+  
+    cotizaciones.forEach(cotizacion => {
+        const carta = crearCartaCotizacion(cotizacion); 
+        container.appendChild(carta); 
+    });
+  };
+  
+  const mostrarGifCarga = () => {
+    document.getElementById('gifCarga').style.display = 'block';
+  };
+  
+  const ocultarGifCarga = () => {
+    document.getElementById('gifCarga').style.display = 'none';
+  };
+  
+  const getCotizaciones = async () => {
+    mostrarGifCarga();
+    const timer = setTimeout(() => {
+      ocultarGifCarga();
+    }, 5000);
+  
+    try {
+      // para usar el server de py descomentar la linea 50
+      const response = await fetch("http://127.0.0.1:5000/"); 
+        // const response = await fetch("https://dolarapi.com/v1/cotizaciones");
+        const data2 = await response.json();
+        const cotizaciones = data2.map(moneda => ({
+            nombre: moneda.nombre,
+            tipoDeCambio: moneda.casa,
+            venta: moneda.venta,
+            compra: moneda.compra
+        }));
+  
+        console.log("cotizacion de la moneda ", cotizaciones);
+        actualizarCotizaciones(cotizaciones); 
+    } catch (error) {
+        console.log("Error al obtener los datos de la cotizacion Euro", error);
+    } finally {
+        clearTimeout(timer);
+        ocultarGifCarga();
+    }
+  };
+  
+  window.onload = getCotizaciones();
---
+++ Server/server.py
@@ -0,0 +1,56 @@
+
+from flask import Flask, jsonify
+from flask_cors import CORS 
+import requests
+
+app = Flask(__name__) #esta  linea sirve para crear una app de flask e indicar __name__ a flask donde se encuenta el archivo principal de nuestro sv corte un main
+CORS(app) #cors nos permite que nuestra app o rutas de flask puedan realizar solicitudes y recibirlas
+@app.route('/dolar', methods=['GET'])
+def get_api_data():  
+    api_url = "https://dolarapi.com/v1/dolares"  
+    response = requests.get(api_url)   
+    print("GET DOLARES")  
+    if response.status_code == 200: #el cod 200 es cuando todo esta OK 
+        data = response.json()
+        info_moneda = [];
+        
+        for i in data:
+            info_moneda.append({
+                'casa': i['casa'],
+                'compra': i['compra'],
+                'venta': i['venta'],
+                'nombre': i['nombre'],
+                'moneda': i['moneda'],
+                'fechaActualizacion': i['fechaActualizacion']
+                
+            })
+        return jsonify(info_moneda)
+    else:
+        return jsonify({'error': 'Paren la rotativa, hay un error...'}), response.status_code
+
+
+@app.route('/', methods=['GET'])
+def get_cotizaciones():  
+    api_url = "https://dolarapi.com/v1/cotizaciones"  
+    response = requests.get(api_url) 
+    print("GET COTIZACIONES")    
+    if response.status_code == 200: #el cod 200 es cuando todo esta OK 
+        data = response.json()
+        info_moneda = [];
+        
+        for i in data:
+            info_moneda.append({
+                'casa': i['casa'],
+                'compra': i['compra'],
+                'venta': i['venta'],
+                'nombre': i['nombre'],
+                'moneda': i['moneda'],
+                'fechaActualizacion': i['fechaActualizacion']
+                
+            })
+        return jsonify(info_moneda)
+    else:
+        return jsonify({'error': 'Paren la rotativa, hay un error...'}), response.status_code
+
+if __name__ == '__main__': #la aplicacion se ejecute solo cuando el archivo se ejecuta directamente, no cuando se importa.
+    app.run(debug=True) # el modo debug en true sirve para que flask te tire los errores detallados si salta algo mal y vuelve a cargar cuando hay cambios en el codigo
